8b12fd3c2c43761eb15e09624f68116ad104d62a
BugTerminator07 <republicofgamerssakin@gmail.com>
2026-03-15 23:44:38 +0600

    adding section done now need some modifications

--- functions.js
+++ functions.js
@@ -12,3 +12,62 @@ function showOnly(id){
     const element=document.getElementById(id);
     element.classList.remove('hidden');
 }
+
+
+function createJobCard(id){
+    // get where to add the card
+    const history=document.getElementById(id);
+    // copy the div from all jobs and add to the history
+   const newHistory=document.createElement('div');
+   if(id=='interview-section'){
+   newHistory.innerHTML=`
+   <div class=" w-full  rounded-xl shadow-sm p-5 mb-5">
+           <div class=" flex flex-col">
+                <h2 class="font-bold text-xl">Software Engineer</h2>
+                <p class="text-gray-500">Google</p>
+                <button class="btn w-[150px] mt-6 btn-success btn-neutral btn-outline interview-button">Interviewed</button>
+                
+            </div>
+            <div class=" flex justify-between items-center mt-5">
+               
+                <p class="text-gray-500">Remote</p>
+                <p class="text-gray-500">Salary: $100,000</p>
+                <p class="text-gray-500">Time: Full-time</p>
+                
+            </div>
+            <div class=" mt-5">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div class=" flex justify-start gap-5 mt-5">
+                <button class="btn btn-success btn-neutral btn-outline interview-button">Interview</button>
+                <button class="btn btn-error btn-outline btn-neutral rejected-button">Rejected</button>
+            </div>
+                </div>` }
+                else if(id=='rejected-section'){
+  newHistory.innerHTML=`
+   <div class=" w-full  rounded-xl shadow-sm p-5 mb-5">
+           <div class=" flex flex-col">
+                <h2 class="font-bold text-xl">Software Engineer</h2>
+                <p class="text-gray-500">Google</p>
+                <button class="btn w-[150px] mt-6 btn-error btn-outline btn-neutral rejected-button">Rejected</button>
+                
+            </div>
+            <div class=" flex justify-between items-center mt-5">
+               
+                <p class="text-gray-500">Remote</p>
+                <p class="text-gray-500">Salary: $100,000</p>
+                <p class="text-gray-500">Time: Full-time</p>
+                
+            </div>
+            <div class=" mt-5">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div class=" flex justify-start gap-5 mt-5">
+                <button class="btn btn-success btn-neutral btn-outline interview-button">Interview</button>
+                <button class="btn btn-error btn-outline btn-neutral rejected-button">Reject</button>
+            </div>
+                </div>`
+                }
+        history.appendChild(newHistory);
+
+}
